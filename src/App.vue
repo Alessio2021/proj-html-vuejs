1b5f2a4a07c0e4d8ee928aedc397476cb344ec88
Alessio2021 <alessiobut1911@gmail.com>
2022-01-24 10:51:08 +0100

    img + bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: "App",
   components: {
     // HelloWorld,
+    Header,
+    Main,
+    Footer,
   },
 };
 </script>
 
 <style lang="scss">
+@import '~bootstrap/scss/bootstrap';
+
 #app {
   // font-family: Avenir, Helvetica, Arial, sans-serif;
   // -webkit-font-smoothing: antialiased;
