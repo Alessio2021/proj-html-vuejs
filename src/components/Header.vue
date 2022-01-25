8e798ec32457d86ec49b02cf2f88bd5b03a872a4
Alessio2021 <alessiobut1911@gmail.com>
2022-01-25 11:19:33 +0100

    stores

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,19 @@
 .<template>
 <header>
+    <!-- fixed-right horizontal -->
+    <section class="horizontal-fixed text-center">
+        <div class="square-fixed">
+            <i class="text-center fas fa-folder-open"></i>
+            <h6>Demos</h6>
+        </div>
+        <div class="square-fixed">
+            <span class="text-success">$</span>
+            <span class="fs-2 text-success">39</span>
+            <h6>On Sale</h6>
+        </div>
+    </section>
     <!-- slider -->
-    <div id="CarouselArea" class="carousel slide position-relative" data-bs-ride="carousel">
+    <div id="CarouselArea" class="carousel slide position-relative w-100" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="../assets/img/hero-07-2x.jpg" class="d-block w-100" alt="...">
@@ -29,7 +41,7 @@
                         <img class="my_img_header ms-5" src="../assets/img/author-logo-round-small.png" alt="">
                     </a>
                 </div>
-                <div class="col-8 d-flex justify-content-end">
+                <div class="col-8">
                     <ul class="list-inline">
                         <li class="list-inline-item">
                             <a href="#">Home</a>
@@ -61,11 +73,28 @@
         </nav>
         <!-- review -->
         <section class="review position-absolute">
-            <div>
-                <img src="" alt="">
+            <h2 class="signature">Damon Vaughn</h2>
+            <h2 class="fontNewYorkTimes">Best-selling author and the most influential public intellectual in the western world right now.</h2>
+            <h5 class="suvaGray">- The New York Time</h5>
+        </section>
+        <!-- new release -->
+        <section class="newRelease position-absolute">
+            <div class="row">
+                <div class="col-5">
+                    <img src="../assets/img/book-widget.png" alt="">
+                </div>
+                <div class="col-7 w-50 pt-3">
+                    <h3>Latest Book Release</h3>
+                    <h5>D.VAUGHN AUTOBIOGRAPHY</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas tempora qui ea magni quae illo. </p>
+                </div>
             </div>
-            <h2>Best-selling author and the most influential public intellectual in the western world right now.</h2>
-            <h5>- The New York Time</h5>
+            <div class="row d-flex justify-content-end">
+                <div class="col text-center">
+                    <a href="#" class="sx"><i class="fab fa-amazon"></i> Buy On Amazon</a>
+                    <a href="#" class="dx"><i class="fab fa-apple"></i> Buy On AppStore</a>
+                </div>
+            </div>
         </section>
     </div>
 </header>
@@ -79,7 +108,11 @@ export default {
 
 <style lang="scss">
 @import "../assets/scss/style.scss";
-
+    .carousel-inner {
+        img {
+            filter: brightness(0.40)
+        }
+    }
     .my-btn-header {
         width: 50px;
         height: 50px;
@@ -118,4 +151,72 @@ export default {
             color: $saffron;
         }
     }
+    .review {
+        bottom: 300px;
+        left: 250px;
+        width: 850px;
+        .signature {
+            font-family: $fontSignature;
+            color: $saffron;
+            font-size: 4em;
+            
+        }
+        .fontNewYorkTimes {
+            font-family: $fontNewYorkTimes;
+        }
+        .suvaGray {
+            color: $suvaGray;
+            font-style: italic;
+        }
+        h2 {
+            font-size: 3em;
+            color: $white;
+
+        }
+    }
+    .newRelease{
+        width: 35%;
+        height: 40%;
+        padding: 2em;
+        background-color: $white;
+        bottom: 0;
+        left: 65%;
+        a {
+            padding: 0.5em 2.5em;
+            background-color: #F5FCFA;
+            border: 2px solid #A4D2C2;
+            text-decoration: none;
+            color: black;
+            box-shadow: 5px 10px 18px #A4D2C2;
+            
+        }
+        a.sx{
+            border-radius: 10px 0 0 10px;
+        }
+        a.dx{
+            border-radius: 0 10px 10px 0;
+        }
+    }
+    .horizontal-fixed {
+        position: fixed;
+        top: 100px;
+        left: 95%;
+        z-index: 1030;
+        .square-fixed {
+            width: 70px;
+            height: 70px;
+            background-color: #ffffff;
+            border-radius: 5px;
+            padding: 5px;
+            margin: 1em;
+            i {
+                color: #33475B;
+                font-size: 2em;
+            }
+            span {
+                line-height: 35px;
+            }
+        }
+    }
+        
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,75 @@
+.<template>
+    <main>
+        <!-- meet The Author -->
+        <section class="meetTheAuthor container my-5">
+            <div class="row">
+                <div class="col text-center">
+                    <h4 class="signature">More About Damon Vaughn</h4>
+                    <p class="fs-3 mt-3">Damon Vaughn is a professor of psicology at the University of Toronto, a clinical psychologist and the author of the multi-million copy bestseller "The story of my life."</p>
+                    <button type="button" class="btn btn-success mt-3 px-4 py-2">Meet The Author</button>
+                </div>
+            </div>
+        </section>
+        <section class="store">
+            <div class="container">
+                <div class="row py-5">
+                    <div class="col-3">
+                        <a href="#">
+                            <img src="../assets/img/ebay-logotan-2x-200x66.png" alt="">
+                        </a>
+                    </div>
+                    <div class="col-3">
+                        <a href="#">
+                            <img src="../assets/img/audible-logotan-2x-200x66.png" alt="">
+                        </a>
+                    </div>
+                    <div class="col-3">
+                        <a href="#">
+                            <img src="../assets/img/bb-logotan-2x-200x66.png" alt="">
+                        </a>
+                    </div>
+                    <div class="col-3">
+                        <a href="#">
+                            <img src="../assets/img/kindlefire-logotan-2x-200x66.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+        <section class="jumbotronAudible">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/style.scss";
+    .meetTheAuthor {
+        padding: 4em;
+        .signature {
+            font-family: $fontSignature;
+            color: $saffron;
+            font-size: 4em;
+        }
+    }
+    .store {
+        background-color: #FEF7E9;
+    }
+    .jumbotronAudible {
+        
+    }
+</style>
