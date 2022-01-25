5077a44b7aeed3b447fc35139296a86c879db92b
Alessio2021 <alessiobut1911@gmail.com>
2022-01-25 12:20:23 +0100

    jumbo audible

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,6 +10,7 @@
                 </div>
             </div>
         </section>
+        <!-- stores -->
         <section class="store">
             <div class="container">
                 <div class="row py-5">
@@ -37,14 +38,45 @@
             </div>
             
         </section>
+        <!-- jumbostron Audiobook -->
         <section class="jumbotronAudible">
             <div class="container">
                 <div class="row">
                     <div class="col-6">
-
+                        <div class="phonePicture"></div>
+                    </div>
+                    <div class="text-jumbo col-6">
+                        <div class="text-center">
+                            <h2 class="fontNewYorkTimes text-light">New Audiobook</h2>
+                            <h5 class="saffron pt-2">NO TIME TO READ? NO PROBLEM. LISTEN TO IT</h5>
+                            <p class="suvaGray pt-2">Expedita consectetur exercitationem nesciunt rem mollitia officiis explicabo ea, quas numquam nostrum </p>
+                            <div class="row pt-2">
+                                <div class="col-4">
+                                    <img src="../assets/img/audible-logo.png" alt="">
+                                </div>
+                                <div class="col-4">
+                                    <img src="../assets/img/bn-logo-tall.png" alt="">
+                                </div>
+                                <div class="col-4">
+                                    <img src="../assets/img/kindle-logo-tall.png" alt="">
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
+            </div>
+        </section>
+        <!-- Testimonials -->
+        <section class="testimonials container">
+            <div class="row">
+                <div class="col-6">
+                    <h2 class="fontNewYorkTimes">What The Crities Say</h2>
+                    <p class="text-uppercase">Testimonials from around the world</p>
+                </div>
+                <div class="col-6 d-flex justify-content-end">
+                    <button type="button" class="btn btn-warning text-light">Read All Testimonials</button>
+                </div>
             </div>
         </section>
     </main>
@@ -58,6 +90,9 @@ export default {
 
 <style lang="scss">
 @import "../assets/scss/style.scss";
+    .fontNewYorkTimes {
+        font-family: $fontNewYorkTimes;
+    }
     .meetTheAuthor {
         padding: 4em;
         .signature {
@@ -70,6 +105,36 @@ export default {
         background-color: #FEF7E9;
     }
     .jumbotronAudible {
+        background-image: url(../assets/img/banner.jpg);
+        height: 670px;
+        background-repeat: no-repeat;
+        background-size: cover;
+        .phonePicture {
+            background-image: url(../assets/img/audible-app-2x-600x628.png);
+            margin-top: 5em;
+            height: 590px;
+            background-repeat: no-repeat;
+            background-size: contain;
+        }
         
+        .suvaGray {
+            color: $suvaGray;
+        }
+        .saffron {
+            color: $saffron;
+        }
+        .text-jumbo {
+            display: flex;
+            align-items: center;
+            padding: 4em;
+        }
+    }
+    .testimonials {
+        margin-top: 5em;
+        button {
+            margin: 2em;
+            padding: 0.5em 2.5em;
+            background-color: $saffron;
+        }
     }
 </style>
